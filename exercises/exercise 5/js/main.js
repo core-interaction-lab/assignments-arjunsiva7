ab94148030f8d983c7708cf33cd84a83084432f2
arjunsiva7 <sivaa730@newschool.edu>
2022-03-04 10:30:41 -0500

    2

--- exercises/exercise 5/js/main.js
+++ exercises/exercise 5/js/main.js
@@ -1,1 +1,21 @@
-alert("this is excersie 5")
+let myButton = document.getElementById('btn-1');/*Only to refernce the ID */
+
+const myButton2 = document.querySelector('.btn');/*more versatile than getElementById*/
+/*you can only declare one name under constant*/
+const allButtons = document.querySelectorAll('.btn');
+
+/*var-vaiables*/
+/*can use multiple ones and it would take the last one*/
+var copy = "this is var copy";
+
+myButton = document.getElementById('btn-2');
+
+console.log(copy);
+
+console.log(myButton);
+console.log(allButtons);
+
+myButton.addEventListener('click', function (event) {
+    alert('I Clicked the button');
+});
+
