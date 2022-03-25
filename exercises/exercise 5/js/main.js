ac7af3d9f1329442b9f5fd626d45cf80f1d5aeeb
arjunsiva7 <sivaa730@newschool.edu>
2022-03-25 10:13:07 -0400

    1

--- exercises/exercise 5/js/main.js
+++ exercises/exercise 5/js/main.js
@@ -19,3 +19,21 @@ myButton.addEventListener('click', function (event) {
     alert('I Clicked the button');
 });
 
+/*let myButton = document.getElementById('btn-1');
+
+const myButton2 = document.querySelector('.btn');
+
+const allButtons = document.querySelectorAll('.btn');
+
+var copy = "this is var copy";
+
+myButton = document.getElementById('btn-2');
+
+console.log(copy);
+
+console.log(myButton);
+console.log(allButtons);
+
+myButton.addEventListener('click', function (event) {
+    alert('I clicked the button');
+});*/
