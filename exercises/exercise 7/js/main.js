4d072e048d2ef16402921ab895118cdefa886ddf
arjunsiva7 <sivaa730@newschool.edu>
2022-03-31 04:13:35 -0400

    1

--- exercises/exercise 7/js/main.js
+++ exercises/exercise 7/js/main.js
@@ -10,25 +10,53 @@ const fetchMovies = async () => {
     const moviesContainer = document.getElementById('movies-container');
 
     /*loop over and add it to the function*/
+    /*runs the same functions for evryething under your list*/
+
     response.records.forEach(movie => {
         console.log(movie.fields); /*for testing purposes*/
         const articleEl = document.createElement('article');
         const titleEl = document.createElement('div');
         const genreEl = document.createElement('div');
-        const imdbEl = document.createElement('div');
+        const imdbEl = document.createElement('a');
         const releaseDateEl = document.createElement('div');
 
-
         titleEl.innerHTML = movie.fields.Title; /*It has to match what's getting returned from airtable, in this case 'Title', dont use any spaces in field titles, use undescores*/
-        // titleEl.innerHTML = movie.fields.Genre; 
+        genreEl.innerHTML = movie.fields.Genre; 
 
-        articleEl.appendChild(titleEl);
+        imdbEl.href = movie.fields.imdb;
+        imdbEl.target = "_blank";
+        imdbEl.classList.add('imdb-link'); //you can add class like this to any field 
+
+        imdbEl.innerHTML = "IMDB page";
+        releaseDateEl.innerHTML = movie.fields.releaseDate;
+        
+        articleEl.append(titleEl, genreEl, releaseDateEl, imdbEl);
         // articleEl.appendChild(genreEl);
 
         moviesContainer.appendChild(articleEl);
         // moviesContainer.appendChild(articleEl);
 
     });
+
+    const linkTags = document.querySelectorAll('.imdb-link');
+    console.log(linkTags); 
+    linkTags.forEach((link, index) =>{
+        const linkColor = link.style.color;
+        link.id = 'my-link-${index+1}';
+
+        //index is number, furst time it runs numder is 0, and then it statrs incremeting 
+        //takes multiple arguements, first few are strings 
+        link.addEventListener('mouseover', (evt) =>{
+            link.style.color = "green";
+        });
+
+        //what happens when you move away the mouse from the object
+        link.addEventListener('mouseout', (evt) =>{
+            link.style.color= linkColor;
+        });
+    });
+
+
 };
 
 fetchMovies();
