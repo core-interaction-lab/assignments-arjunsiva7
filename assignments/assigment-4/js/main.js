fd927c0c49c441da9b846d03f0409255fc780eac
arjunsiva7 <sivaa730@newschool.edu>
2022-04-26 05:24:21 -0400

    2

--- assignments/assigment-4/js/main.js
+++ assignments/assigment-4/js/main.js
@@ -9,24 +9,17 @@ const episodes = episodes => {
     episodes.forEach(item=> {
         console.log(item);
         lbreak = document.createElement("br");
-        Season = document.createElement("div");
-
 
         const imgEl = document.createElement('img');
         const summaryEl = document.createElement('div');
         const nameEl = document.createElement('div');
-        const airstampEl = document.createElement('div');
-
 
         summaryEl.innerHTML = item.summary; 
         nameEl.innerHTML = item.name;
-        airstampEl.innerHTML = item.airstamp;
 
         summaryEl.classList.add('sum');
         nameEl.classList.add('name');
         imgEl.classList.add('img');
-        airstampEl.classList.add('air');
-
 
         imgEl.setAttribute('src', item.image.medium);
         wire.append(nameEl,imgEl,summaryEl);
