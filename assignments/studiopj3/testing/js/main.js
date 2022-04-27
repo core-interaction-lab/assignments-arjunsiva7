2945e8bb57918cde179dec1e0cf9b7e86e1a1ecf
arjunsiva7 <sivaa730@newschool.edu>
2022-04-26 21:39:00 -0400

    2

--- assignments/studiopj3/testing/js/main.js
+++ assignments/studiopj3/testing/js/main.js
@@ -2,10 +2,6 @@ const slideContainer = document.getElementById('right-container');
 
 const fetchGames = async () => {
     const response = await fetch("https://api.airtable.com/v0/appVvOsHOLrzipYpV/Table%201?maxRecords=100&api_key=keyA0jnAuvBfcVUVl").then(data => data.json()); 
-    // console.log(response);
-    // https://api.airtable.com/v0/appVvOsHOLrzipYpV/Table%201?maxRecords=100&api_key=keyA0jnAuvBfcVUVl
-
-    // const moviesContainerTwo = document.getElementById('movies-container-two');
 
     Array.from(document.querySelectorAll('.select-item-btn')).map(btn => {
         btn.addEventListener('click', evt => {
@@ -27,7 +23,6 @@ const fetchGames = async () => {
             nameEl.classList.add('charname');
             descriptionEl.classList.add('des');
 
-
             slideContainer.innerHTML = '';
             slideContainer.append(itemContainer);
 
@@ -48,48 +43,6 @@ const fetchGames = async () => {
 
         })
     });
-
-//     response.records.forEach(games => {
-//         linebreak = document.createElement("br");
-//         lbreak = document.createElement("br");
-        
-//         const articleEl = document.createElement('article');
-//         const articleElTwo = document.createElement('article');
-//         const NameEl = document.createElement('div');
-//         const descriptionEl = document.createElement('div');
-//         const GameEl = document.createElement('div');
-//         const charstatEl = document.createElement('div');
-
-//         NameEl.classList.add('name');
-//         GameEl.classList.add('game');
-//         descriptionEl.classList.add('disc');
-//         charstatEl.classList.add('stat');
-
-//         NameEl.innerHTML = games.fields.Name; 
-//         descriptionEl.innerHTML = games.fields.description;
-//         GameEl.innerHTML = games.fields.Game; 
-//         charstatEl.innerHTML = games.fields.charstat; 
-
-//         const photoImg = document.createElement('img');
-//         photoImg.classList.add('image-styling');
-
-//         response.records.forEach((image) => {
-//             // console.log(image);
-//             if(games.fields.image){
-//                 photoImg.src = games.fields.image[0].url;
-
-//                 articleElTwo.append(photoImg);
-//                 photoImg.style.width = '200px';
-//             }
-//         });
-
-//         articleEl.append(lbreak,photoImg,lbreak,lbreak,NameEl,GameEl,lbreak,lbreak,descriptionEl,charstatEl);
-//         // articleElTwo.append(photoImg,lbreak);
-
-//         moviesContainer.appendChild(articleEl); 
-//         // gamesContainerTwo.appendChild(articleElTwo);
-
-//     });
 
 };
 
--- assignments/studiopj3/testing/js/two.js
+++ assignments/studiopj3/testing/js/two.js
@@ -1,57 +1,30 @@
 const fetchGames = async () => {
-    const response = await fetch("https://api.airtable.com/v0/appVvOsHOLrzipYpV/Table%201?maxRecords=1&api_key=keyA0jnAuvBfcVUVl").then(data => data.json()); 
-    // console.log(response);
+    const response = await fetch("https://api.airtable.com/v0/appVvOsHOLrzipYpV/Table%201?maxRecords=100&api_key=keyA0jnAuvBfcVUVl").then(data => data.json()); 
+    console.log(response);
 
-    // https://api.airtable.com/v0/appVvOsHOLrzipYpV/Table%201?maxRecords=100&api_key=keyA0jnAuvBfcVUVl
-
-    const moviesContainer = document.getElementById('movies-container');
-    // const moviesContainerTwo = document.getElementById('movies-container-two');
+    const moviesContainer = document.getElementById('gamesContainer');
 
     response.records.forEach(games => {
-        linebreak = document.createElement("br");
-        lbreak = document.createElement("br");
-        line = document.createElement("hr");
+        // Array.from(document.querySelectorAll('.select-item-btn')).map(btn => {
+        //     btn.addEventListener('click', evt => {
+        //         const record = response.records[btn.dataset.itemIndex];
+                    linebreak = document.createElement("br");
+                    lbreak = document.createElement("br");
+                    line = document.createElement("hr");
 
-        const articleEl = document.createElement('article');
-        const articleElTwo = document.createElement('article');
-        const NameEl = document.createElement('div');
-        const descriptionEl = document.createElement('div');
-        const GameEl = document.createElement('div');
-        const charstatEl = document.createElement('div');
-        const lineEl = document.createElement('div');
-        // const gameEl= 'game';
-
-        NameEl.classList.add('name');
-        GameEl.classList.add('game');
-        descriptionEl.classList.add('disc');
-        charstatEl.classList.add('stat');
-        lineEl.classList.add('lone');
-        // gameEl.classList.add('game');
-
-        NameEl.innerHTML = games.fields.Name; 
-        descriptionEl.innerHTML = games.fields.description;
-        GameEl.innerHTML = games.fields.Game; 
-        charstatEl.innerHTML = games.fields.charstat; 
+                    const articleEl = document.createElement('article');
+                    const articleElTwo = document.createElement('article');
+                    const NameEl = document.createElement('div');
 
 
-        const photoImg = document.createElement('img');
-        photoImg.classList.add('image-styling');
+                    NameEl.classList.add('name');
+                    NameEl.innerHTML = games.fields.Name; 
 
-        response.records.forEach((image) => {
-            // console.log(image);
-            if(games.fields.image){
-                photoImg.src = games.fields.image[0].url;
+                    articleEl.append(NameEl,lbreak);
+                    // articleElTwo.append(photoImg,lbreak);
 
-                articleElTwo.append(photoImg);
-                photoImg.style.width = '200px';
-            }
-        });
-
-        articleEl.append(linebreak,photoImg,NameEl,GameEl,linebreak,linebreak,lineEl,descriptionEl);
-        // articleElTwo.append(photoImg,lbreak);
-
-        gamesContainer.appendChild(articleEl); 
-        // gamesContainerTwo.appendChild(articleElTwo);
+                    gamesContainer.appendChild(articleEl); 
+                    // gamesContainerTwo.appendChild(articleElTwo);
 
     });
 
