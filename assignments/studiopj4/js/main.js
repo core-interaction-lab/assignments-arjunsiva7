2f82b57ecd1a7956dbb06a162cf94791dc8fe406
arjunsiva7 <sivaa730@newschool.edu>
2022-05-03 22:06:30 -0400

    2

---
+++ assignments/studiopj4/js/main.js
@@ -0,0 +1,24 @@
+// const togglePassword = document.getElementById('togglePassword');
+
+// const showOrHidePassword = () => {
+//   const password = document.getElementById('password');
+//   if (password.type === 'password') {
+//     document.getElementById ('show').style.display = 'block';
+//   } else {
+//     document.getElementById ('show').style.display = 'none';
+//   }
+// };
+
+// togglePassword.addEventListener('change', showOrHidePassword);
+
+function check_password (input_element) {
+  
+    var password = input_element.value;
+    
+    if (password == 'abcd')
+        document.getElementById ('show').style.display = 'block';
+    else
+        document.getElementById ('show').style.display = 'none';
+  
+  }
+  
