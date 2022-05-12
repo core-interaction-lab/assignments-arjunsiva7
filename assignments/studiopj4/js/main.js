bbad38f206a165de3052467f4bcea572903366f1
arjunsiva7 <sivaa730@newschool.edu>
2022-05-12 18:50:57 -0400

    2

--- assignments/studiopj4/js/main.js
+++ assignments/studiopj4/js/main.js
@@ -11,14 +11,20 @@
 
 // togglePassword.addEventListener('change', showOrHidePassword);
 
-function check_password (input_element) {
-  
-    var password = input_element.value;
-    
-    if (password == 'abcd')
-        document.getElementById ('show').style.display = 'block';
+const passwordForm = document.getElementById('password-form');
+const passwordField = document.getElementById('password-field');
+
+passwordForm.onsubmit = check_password;
+
+function check_password (evt) {
+  evt.preventDefault();
+    var password = passwordField.value;
+    console.log(password)
+    if (password == 'realitycheck')
+        document.getElementById ('hidden_div').style.display = 'block';
     else
-        document.getElementById ('show').style.display = 'none';
-  
+        document.getElementById ('hidden_div').style.display = 'none';
 }
+
+
   
